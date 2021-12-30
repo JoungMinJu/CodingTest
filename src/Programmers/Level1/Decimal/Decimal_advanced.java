18f774dcee84f9e300693f964c9f16537283a941
MINJU <alswn9938@gmail.com>
2021-12-30 18:37:48 +0900

    [lv1.Decimal] 참조코드

---
+++ src/Programmers/Level1/Decimal/Decimal_advanced.java
@@ -0,0 +1,27 @@
+package Programmers.Level1.Decimal;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Decimal_advanced {
+    public int solution(int[] nums) {
+        int answer = -1;
+        for(int i= 0;i<nums.length -2;i++){
+            for(int j = i+1;j<nums.length -1; j++){
+                for(int k =j+1;k<nums.length;k++){
+                    if(isPrime(nums[i]+nums[j]+nums[k])){
+                     answer+=1;}
+                }
+            }
+        }
+        return answer;
+    }
+    // 맨날 까먹는 실수판별 알고리즘
+    public Boolean isPrime(int num){
+        int cnt =0;
+        for(int i= 1;i<=(int)Math.sqrt(num);i++){
+            if(num%i == 0) cnt +=1;
+        }
+        return cnt == 1;
+    }
+}
+
