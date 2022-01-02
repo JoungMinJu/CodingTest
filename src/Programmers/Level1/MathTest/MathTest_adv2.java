b1734d184d95a517213d53582a6beb50bd2bbeed
MINJU <alswn9938@gmail.com>
2022-01-02 14:29:44 +0900

    [lv1.MathTest] 참조코드

---
+++ src/Programmers/Level1/MathTest/MathTest_adv2.java
@@ -0,0 +1,31 @@
+package Programmers.Level1.MathTest;
+import java.util.*;
+
+public class MathTest_adv2 {
+    public static int[] solution(int[] answers){
+        int[][] patterns = {
+                {1,2,3,4,5},
+                {2,1,2,3,2,4,2,5},
+                {3,3,1,1,2,2,4,4,5,5}};
+        int[] hit = new int[3];
+        for(int i=0;i<hit.length;i++){
+            for(int j=0;j<answers.length;i++){
+                if(patterns[i][j % patterns[i].length]==answers[j]) hit[i]++;
+            }
+        }
+        int max=Math.max(hit[0],Math.max(hit[1],hit[2]));
+        List<Integer> list  = new ArrayList<>();
+        for(int i=0;i<hit.length;i++){
+            if(max==hit[i]) list.add(i+1);
+        }
+
+
+        //Integer ->int로 변환하는 다른 방법
+        int[] answer = new int[list.size()];
+        int cnt = 0;
+        for(int num:list){
+            answer[cnt++]=num;
+        }
+        return answer;
+    }
+}
