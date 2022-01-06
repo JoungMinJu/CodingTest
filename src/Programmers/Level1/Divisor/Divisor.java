b83e0427c69dc79f6bfe53bc7c4dcce49e0305fd
MINJU <alswn9938@gmail.com>
2022-01-07 01:35:47 +0900

    [lv1.Divisor] 약수 풀이

---
+++ src/Programmers/Level1/Divisor/Divisor.java
@@ -0,0 +1,22 @@
+package Programmers.Level1.Divisor;
+
+public class Divisor {
+    public int solution(int left, int right){
+        int answer = 0;
+        //약수 개수 구하기
+        for(int i = left; i<=right;i++){
+            int count = 0;
+            for(int j =1; j*j<=i;j++){
+                if(j*j == i) count++;
+                else if(i%j ==0) count +=2;
+            }
+            if(count % 2 ==0){
+                answer +=i;
+            }
+            else{
+                answer-=i;
+            }
+        }
+        return answer;
+    }
+}
