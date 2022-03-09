18066e8437b93639388e4fe7d7c0bdf07764e2a5
MINJU <alswn9938@gmail.com>
2022-03-09 23:20:45 +0900

    [lv1.PhoneNumber] PhoneNumber 풀이 및 참조

---
+++ src/Programmers/Level2/PhoneNumber/PhoneNumber_adv.java
@@ -0,0 +1,20 @@
+package Programmers.Level2.PhoneNumber;
+
+import java.util.HashMap;
+
+public class PhoneNumber_adv {
+    public boolean solution(String[] phone_book){
+        boolean answer = true;
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i=0;i<phone_book.length;i++){
+            map.put(phone_book[i],i);
+        }
+        for(int i=0;i< phone_book.length; i++){
+            for(int j=1;j<phone_book[i].length();j++){
+                if(map.containsKey(phone_book[i].substring(0,j))) return false;
+                return answer;
+            }
+        }
+        return answer;
+    }
+}
