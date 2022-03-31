c56b05046fa0eca4e109ec37de06aaa1c3a1a1ba
MINJU <alswn9938@gmail.com>
2022-03-31 13:53:03 +0900

    [lv2.BOJ_18870] BOJ_18870 풀이 및 참조

--- src/BaekJoon/Sort/BOJ_18870.java
+++ src/BaekJoon/Sort/BOJ_18870.java
@@ -4,57 +4,40 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+// O(N제곱 알고리즘)이 된다..응
 
 public class BOJ_18870 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         ArrayList<Integer> arr = new ArrayList<>();
-        ArrayList<Integer> tmp = new ArrayList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
         HashSet<Integer> s = new HashSet<>();
         for(int i=0;i<n;i++){
             arr.add(Integer.parseInt(st.nextToken()));
-            // tmp도 arraylist로 하고 index 찾고 삭제해버리면 되겠다.
-            tmp.add(arr.get(i));
+        }
+        // Hash set에 넣고 set을 정렬하기
+        for(int i=0;i<n;i++){
             s.add(arr.get(i));
         }
+        // hashset 정렬
+        ArrayList<Integer> lst = new ArrayList<>(s);
+        Collections.sort(lst);
 
-        // 정렬하기
-        ArrayList<Integer> list = new ArrayList<>(s);
-        Collections.sort(list);
-        //999, 1000으로 list 배열 정렬
+        Map<Integer, Integer> indexList = new HashMap<>();
 
-        Set<Integer> indexSet;
-        // 큰애부터 꺼냄
-        for(int i=list.size()-1;i>=0;i--){
-            // t = 1000
-            int t = list.get(i);
-
-            // 1000의 모든 인덱스 추출
-            indexSet = new HashSet<Integer>();
-            // tmp에서 1000에 해당하는 index추출
-            int index;
-            int start=0;
-            while(true){
-                index = tmp.subList(start, tmp.size()).indexOf((Integer)t);
-                System.out.println("t = " + t);
-                System.out.println("index = " + index);
-                if(index>=tmp.size()) break;
-                if(index == -1) break;
-                if(!indexSet.contains((Integer) index)) {indexSet.add(index); start=index+1;}
-                System.out.println("start = " + start);
-                }
-
-            // 추출한 index에 값 넣어주기
-            list.remove((Integer) t);
-            for (Integer integer : indexSet) {
-                arr.set(integer, list.size());
-            }
+        int rank = 0;
+        for(int i=0;i<lst.size();i++){
+            indexList.put(lst.get(i), rank);
+            rank++;
         }
 
-        for (Integer integer : arr) {
-            System.out.println("integer = " + integer);
+        for(int i=0;i<n;i++){
+            ans.add(indexList.get(arr.get(i)));
+        }
+        for (Integer an : ans) {
+            System.out.print(an+" ");
         }
 
     }
