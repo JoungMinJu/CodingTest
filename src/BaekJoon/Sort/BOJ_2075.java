e16039889e2f7043c9f74744701a9e082ed85ca3
Joung Min Ju <alswn9938@gmail.com>
2022-05-02 10:09:14 +0900

    [BOJ_1927] BOJ_1927 풀이 및 참조

---
+++ src/BaekJoon/Sort/BOJ_2075.java
@@ -0,0 +1,16 @@
+package BaekJoon.Sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2075 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int map[][] = new int[n][n];
+        for(int i=0;i<n;i++){
+
+        }
+    }
+}
