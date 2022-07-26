7e2a591980a97010a34ebf67d10f217dad423cd7
Joung Min Ju <alswn9938@gmail.com>
2022-07-26 13:43:45 +0900

    [BOJ_12904] BOJ_12904 풀이 및 참조

---
+++ src/study/second/BOJ_12904.java
@@ -0,0 +1,40 @@
+package study.second;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class BOJ_12904 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Deque<Character> dq = new LinkedList<>();
+        String s = br.readLine();
+        String t = br.readLine();
+        for(int i=0; i<t.length(); i++)
+            dq.add(t.charAt(i));
+        boolean is_flip = false;
+        while(dq.size() != s.length()){
+            char tmp = is_flip ? dq.getFirst() : dq.getLast();
+            if(tmp == 'A'){
+                if(is_flip) dq.removeFirst();
+                else dq.removeLast();
+            }
+            else if(tmp == 'B'){
+                if(is_flip) dq.removeFirst();
+                else dq.removeLast();
+                is_flip = !is_flip;
+            }
+        }
+        String temp = "";
+        while(!dq.isEmpty()){
+            if(is_flip)
+                temp += "" + dq.pollLast();
+            else
+                temp += "" + dq.pollFirst();
+        }
+        int answer = temp.equals(s)? 1:0;
+        System.out.println(answer);
+    }
+}
