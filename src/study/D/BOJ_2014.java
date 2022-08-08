af434f6295e71318500c7382dac0002237e31bbd
Joung Min Ju <alswn9938@gmail.com>
2022-08-08 11:25:10 +0900

    [BOJ_2014] BOJ_2014 풀이 및 참조

--- src/study/D/BOJ_2014.java
+++ src/study/D/BOJ_2014.java
@@ -3,19 +3,33 @@ package study.D;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class BOJ_2014 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        PriorityQueue<Long> q  = new PriorityQueue<>();
+
         int k  = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
+        long[] arr = new long[k];
 
-        int[] prime = new int[n];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<k; i++)
-            prime[i] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<k; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            q.add(arr[i]);
+        }
 
+        for(int i=1; i<n; i++){
+            Long poll = q.poll();
+            for(int j=0; j<k; j++){
+                long tmp = poll * arr[j];
+                q.add(tmp);
+                if(poll % arr[j] == 0 ) break;
+            }
+        }
+        System.out.println(q.poll());
     }
 }
