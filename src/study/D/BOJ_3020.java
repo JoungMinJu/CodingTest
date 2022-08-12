3181b6581f95ecf8867d37c79581a17d9eebe60c
Joung Min Ju <alswn9938@gmail.com>
2022-08-12 15:17:29 +0900

    [BOJ_3020] BOJ_3020 풀이 및 참조

---
+++ src/study/D/BOJ_3020.java
@@ -0,0 +1,20 @@
+package study.D;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_3020 {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        int n = Integer.parseInt(st.nextToken());
+//        int h = Integer.parseInt(st.nextToken());
+//
+//        // 석순 -> 종유석 -> 석순 -> 종유석
+//
+        System.out.println((int)(7/2));
+    }
+}
