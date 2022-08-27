0848df1bed0bb81b61b684b801f02419fe5f2899
MINJU <alswn9938@gmail.com>
2022-08-27 13:35:53 +0900

    [BOJ_1837] BOJ_1837 풀이 및 참조

---
+++ src/study/H/BOJ_1837.java
@@ -0,0 +1,33 @@
+package study.H;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class BOJ_1837 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BigInteger p = new BigInteger(st.nextToken());
+        BigInteger k = new BigInteger(st.nextToken());
+
+        BigInteger i = new BigInteger("1");
+
+        while(true){
+            if(i.compareTo(k) == 1 || i.compareTo(i)==0){
+                System.out.println("GOOD");
+                break;
+            }
+            i = i.add(BigInteger.ONE);
+
+            if(p.remainder(i).compareTo(BigInteger.ZERO) == 0 && i.compareTo(k) == -1){
+                System.out.println("BAD " + i);
+                break;
+            }
+        }
+
+    }
+}
