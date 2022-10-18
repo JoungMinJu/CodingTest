7a616a24e504eeb40bbfd5605a4ec9b131e30a62
alswn <alswn9938@gmail.com>
2022-10-18 15:07:59 +0900

    [BOJ_11003] BOJ_11003 풀이

---
+++ src/Doit/C/BOJ_11003.java
@@ -0,0 +1,40 @@
+package DoIt.C;
+
+import java.io.*;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class BOJ_11003 {
+    static class Node{
+        int value;
+        int index;
+
+        public Node(int index, int value){
+            this.index = index;
+            this.value = value;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        Deque<Node> dq = new LinkedList<>();
+
+        for(int i=0; i<n; i++){
+            int now = Integer.parseInt(st.nextToken());
+            while(!dq.isEmpty() && dq.getLast().value > now)
+                dq.removeLast();
+            dq.add(new Node(i, now));
+            // 범위 체크
+            if(i - dq.getFirst().index >= l)
+                dq.removeFirst();
+            bw.write(dq.getFirst().value + " ");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
---
+++ src/Doit/C/BOJ_12891.java
@@ -0,0 +1,102 @@
+package DoIt.C;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class BOJ_12891 {
+    static int S, P, ans;
+    static char dna[];
+    static int minA, minC, minG, minT;
+    static int cntA, cntC, cntG, cntT;
+
+    private static void check() {
+        if (minA <= cntA && minC <= cntC && minG <= cntG && minT <= cntT) ans++;
+    }
+
+    private static void solve() {
+        // 초기값 갱신
+        for (int i = 0; i < P; i++) {
+            switch (dna[i]) {
+                case 'A':
+                    cntA++;
+                    break;
+                case 'C':
+                    cntC++;
+                    break;
+                case 'G':
+                    cntG++;
+                    break;
+                case 'T':
+                    cntT++;
+                    break;
+            }
+        }
+
+        // 최소 문자수 확인 카운팅
+        check();
+
+        // 슬라이딩 윈도우 사용
+        for (int i = P; i < S; i++) {
+            // 초기값 뺴주기
+            switch (dna[i - P]) {
+                case 'A':
+                    cntA--;
+                    break;
+                case 'C':
+                    cntC--;
+                    break;
+                case 'G':
+                    cntG--;
+                    break;
+                case 'T':
+                    cntT--;
+                    break;
+            }
+
+            // 새로 들어오는 값 더해주기
+            switch (dna[i]) {
+                case 'A':
+                    cntA++;
+                    break;
+                case 'C':
+                    cntC++;
+                    break;
+                case 'G':
+                    cntG++;
+                    break;
+                case 'T':
+                    cntT++;
+                    break;
+            }
+
+            check();
+        }
+    } // end solve
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 변수 입력
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        S = Integer.parseInt(st.nextToken());
+        P = Integer.parseInt(st.nextToken());
+
+        // 배열 입력
+        dna = new char[S];
+        dna = br.readLine().toCharArray();
+
+        // 만족해야하는 최소의 갯수
+        st = new StringTokenizer(br.readLine());
+        minA = Integer.parseInt(st.nextToken());
+        minC = Integer.parseInt(st.nextToken());
+        minG = Integer.parseInt(st.nextToken());
+        minT = Integer.parseInt(st.nextToken());
+
+        solve();
+        System.out.println(ans);
+    } // end main
+}
