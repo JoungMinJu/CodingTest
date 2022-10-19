780fe9ae91f9febb7eee07a02535d54f1d992253
alswn <alswn9938@gmail.com>
2022-10-19 13:12:03 +0900

    [BOJ_17298] BOJ_17298 풀이

---
+++ src/Doit/C/BOJ_17298.java
@@ -0,0 +1,39 @@
+package DoIt.C;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class BOJ_17298 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int arr[] = new int[N];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=1; i<N; i++){
+            while(!stack.isEmpty() && arr[stack.peek()] < arr[i]){
+                // 기존 원소가 현재 값보다 작으면
+                arr[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+        while(!stack.isEmpty()){
+            arr[stack.pop()] = -1;
+        }
+        for(int i=0; i<N; i++){
+            sb.append(arr[i]+ " ");
+        }
+        System.out.println(sb.toString());
+    }
+}
