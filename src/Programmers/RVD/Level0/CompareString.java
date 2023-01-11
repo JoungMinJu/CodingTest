4338d464efc1e6b16b6ba1e488663d76b8e8ebb5
MINJU <alswn9938@gmail.com>
2023-01-11 18:16:19 +0900

    add : Level0 : CompareString

---
+++ src/Programmers/RVD/Level0/CompareString.java
@@ -0,0 +1,17 @@
+package Programmers.RVD.Level0;
+
+import java.util.Arrays;
+
+public class CompareString {
+    public int solution(String before, String after){
+        char[] a = before.toCharArray();
+        char[] b = after.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return new String(a)
+                .equals(new String(b))
+                ? 1 : 0;
+    }
+}
