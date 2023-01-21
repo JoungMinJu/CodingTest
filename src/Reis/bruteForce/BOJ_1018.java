7e4262b361f11f88bde2996d011d8668fad5c2ba
MINJU <alswn9938@gmail.com>
2023-01-22 00:27:25 +0900

    refactor : BF : BOJ_1018

--- src/Reis/bruteForce/BOJ_1018.java
+++ src/Reis/bruteForce/BOJ_1018.java
@@ -17,6 +17,7 @@ public class BOJ_1018 {
         int m = Integer.parseInt(st.nextToken());
         char map[][] = new char[n][m];
         int answer = 50 * 50 + 1;
+        int tmp = 0;
 
         for (int i = 0; i < n; i++) {
             String input = br.readLine();
@@ -25,46 +26,27 @@ public class BOJ_1018 {
             }
         }
 
-        for (int i = 0; i <= n - 8 ; i++) {
-            for (int j = 0; j <= m - 8 ; j++) {
-                answer = Math.min(answer, getAnswer(map.clone(), i, j, i + 8, j + 8));
+        for (int i = 0; i <= n - 8; i++) {
+            for (int j = 0; j <= m - 8; j++) {
+                tmp = Math.min(getAnswer('W', map, i, j, i + 8, j + 8), getAnswer('B', map, i, j, i + 8, j + 8));
+                answer = Math.min(answer, tmp);
             }
         }
         System.out.println(answer);
     }
 
-    private static int getAnswer(char[][] map, int startX, int startY, int endX, int endY) {
-        int firstCount = 0, secondCount = 0;
-        boolean isFirstRow = true;
-        // 확인해보기
-        int index = order.indexOf(map[startX][startY]);
-        for (int i = startX + 1; i < endX; i++) {
-            if (order.charAt((index + 1) % 2) != map[i][startY]) {
-                firstCount++;
-            }
-            index++;
-        }
-
-        index = order.indexOf(map[startX][endY - 1]);
-        for (int i = startX + 1; i < endX; i++) {
-            if (order.charAt((index + 1) % 2) != map[i][endY - 1]) {
-                secondCount++;
-            }
-            index++;
-        }
-
-        if (firstCount <= secondCount){
-            return getAnswerFirstColumn(map, startX, startY, endX, endY);
-        }
-        return getAnswerLastColumn(map, startX, startY, endX, endY);
-    }
-
-    private static int getAnswerFirstColumn(char[][] map, int startX, int startY, int endX, int endY) {
+    private static int getAnswer(char startChar, char[][] map, int startX, int startY, int endX, int endY) {
         int count = 0, index;
         List<List<Integer>> changeIndexes = new ArrayList<>();
 
         // 첫번쨰 열 맞추기
         index = order.indexOf(map[startX][startY]);
+        if (startChar != map[startX][startY]) {
+            map[startX][startY] = order.charAt((index += 1) % 2);
+            count++;
+            changeIndexes.add(List.of(startX, startY));
+        }
+
         for (int i = startX + 1; i < endX; i++) {
             if (order.charAt((index + 1) % 2) != map[i][startY]) {
                 count++;
@@ -96,40 +78,4 @@ public class BOJ_1018 {
         return count;
     }
 
-    private static int getAnswerLastColumn(char[][] map, int startX, int startY, int endX, int endY) {
-        int count = 0, index;
-        List<List<Integer>> changeIndexes = new ArrayList<>();
-
-        // 마지막열
-        index = order.indexOf(map[startX][endY - 1]);
-        for (int i = startX + 1; i < endX; i++) {
-            if (order.charAt((index + 1) % 2) != map[i][endY - 1]) {
-                count++;
-                changeIndexes.add(List.of(i, endY - 1));
-                map[i][endY - 1] = order.charAt((index + 1) % 2);
-            }
-            index++;
-        }
-
-        // 나머지 열 맞추기
-        for (int i = startX; i < endX; i++) {
-            index = order.indexOf(map[i][endY - 1]);
-            for (int j = endY - 2; j >= startY; j--) {
-                if (order.charAt((index + 1) % 2) != map[i][j]) {
-                    count++;
-                    changeIndexes.add(List.of(i, j));
-                    map[i][j] = order.charAt((index + 1) % 2);
-                }
-                index++;
-            }
-        }
-
-        for (List<Integer> changeIndex : changeIndexes) {
-            int nowX = changeIndex.get(0);
-            int nowY = changeIndex.get(1);
-            index = order.indexOf(map[nowX][nowY]);
-            map[nowX][nowY] = order.charAt((index + 1) % 2);
-        }
-        return count;
-    }
 }
