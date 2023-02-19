f1f92ad2b94e5abcf0d2bd26f2818952bc81e64a
MINJU <alswn9938@gmail.com>
2023-02-19 21:43:37 +0900

    add(Reis) : Stack : BOJ_1018

---
+++ src/Reis/stack/BOJ_1918.java
@@ -0,0 +1,57 @@
+package Reis.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BOJ_1918{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String input = br.readLine();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i= 0; i<input.length(); i++){
+            char now = input.charAt(i);
+
+            switch (now) {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                    while (!stack.isEmpty() && priority(stack.peek()) >= priority(now)) {
+                        sb.append(stack.pop());
+                    }
+                    stack.add(now);
+                    break;
+                case '(':
+                    stack.add(now);
+                    break;
+                case ')':
+                    while(!stack.isEmpty() && stack.peek() != '(') {
+                        sb.append(stack.pop());
+                    }
+                    stack.pop();
+                    break;
+                default:
+                    sb.append(now);
+            }
+        }
+
+        while ( ! stack.isEmpty())
+            sb.append(stack.pop());
+        System.out.println(sb.toString());
+    }
+
+    private static int priority(char operator){
+        if (operator =='(' || operator == ')')
+            return 0;
+        else if (operator == '+' || operator == '-')
+            return 1;
+        else if (operator == '*' || operator == '/')
+            return 2;
+        return -1;
+    }
+}
