0415fafd7a98842613781709eed287ff64e1211a
alswn <alswn9938@gmail.com>
2023-04-17 16:12:46 +0900

    add(Programmers) : ShootingSystem

---
+++ src/Programmers/plz/ShootingSystem.java
@@ -0,0 +1,25 @@
+package Programmers.plz;
+
+import java.util.Arrays;
+
+// 요격시스템
+public class ShootingSystem {
+    public int solution(int[][] targets) {
+        int answer = 0;
+        Arrays.sort(targets, (o1, o2) -> {return o1[1]-o2[1];});
+        int last = -1;
+        for (int[] target : targets) {
+            if (last == -1) {
+                answer ++;
+                last = target[1] - 1;
+                continue;
+            }
+            if (last >= target[0] && last <= target[1]) {
+                continue;
+            }
+            answer ++;
+            last = target[1] - 1;
+        }
+        return answer;
+    }
+}
